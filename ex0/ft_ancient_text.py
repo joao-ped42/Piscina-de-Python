442c5dceee87b67f13b1c93a574cfc30ccbcdf31
João Pedro <joaopedrorodrigues1333@gmail.com>
2026-03-18 16:58:35 -0300

    conserto

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,19 @@
+def main() -> None:
+    try:
+        print("Accessing Storage Vault: ancient_fragment.txt")
+        file: open = open("../data-generator-tools/ancient_fragment.txt",
+                          "r")
+        text: str = file.read()
+        print("Connection established...\n")
+        print("RECOVERED DATA")
+        print(text)
+        print("\nData recovery complete. Storage unit disconnected.")
+    except Exception as error:
+        print(f"An error occurred: {error}")
+    finally:
+        file.close()
+
+
+if (__name__ == "__main__"):
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    main()
---
+++ ex2/ft_stream_management.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def main() -> None:
+    print("Input Stream active. Enter archivist ID: ",
+          end="", flush=True)
+    arch_id: str = sys.stdin.readline().strip()
+    print("Input Stream active. Enter status report: ",
+          end="", flush=True)
+    status_report: str = sys.stdin.readline().strip()
+    print(f"\n[STANDARD] Archive status from {arch_id}: {status_report}",
+          file=sys.stdout)
+    print("[ALERT] System diagnostic: Communication channels verified",
+          file=sys.stderr)
+    print("[STANDARD] Data transmission complete",
+          file=sys.stdout)
+
+
+if (__name__ == "__main__"):
+    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    print("\nThree-channel communication test successful.")
