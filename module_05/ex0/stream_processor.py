09c4cac819ac4df9c4a7189846490da0670a8f1b
joao-ped42 <joaopedrorodrigues1333@gmail.com>
2026-03-20 01:01:31 -0300

    agora sim

--- module_05/ex0/stream_processor.py
+++ module_05/ex0/stream_processor.py
@@ -49,7 +49,6 @@ class DataProcessor(ABC):
 
 class NumericProcessor(DataProcessor):
     def process(self, data: Any) -> str:
-        print(f"Processing data: {data}")
         return (f"Processed {ft_len(data)} numeric valuers, "
                 f"sum={ft_sum(data)}, avg={ft_avg(data)}")
 
@@ -62,7 +61,6 @@ class NumericProcessor(DataProcessor):
 
 class TextProcessor(DataProcessor):
     def process(self, data: Any) -> str:
-        print(f'Processing data: "{data}"')
         return (f"Processed text: {ft_len(data)} characters, "
                 f"{count_words(data)} words")
 
@@ -75,9 +73,7 @@ class TextProcessor(DataProcessor):
 
 class LogProcessor(DataProcessor):
     def process(self, data: Any) -> str:
-        print(f"Processing data: {data}")
-        return (f"Processed {ft_len(data)} numeric valuers, "
-                f"sum={ft_sum(data)}, avg={ft_avg(data)}")
+        return ("[INFO] INFO level detected: System ready")
 
     def validate(self, data: Any) -> bool:
         if (data is None):
@@ -94,23 +90,40 @@ def print_message(data: Any, cls: DataProcessor) -> None:
     else:
         print("\nInitializing Log Processor...")
     try:
-        processed: str = cls.process(cls, data)
-        cls.validate(cls, data)
+        print(f"Processing data: {data}")
+        processed: str = cls.process(data)
+        cls.validate(data)
     except Exception as error:
         print(f"Validation: [ERROR] {error}")
         processed: str = ("[ALERT] ERROR level detected: Connection timed out")
     finally:
-        output: str = cls.format_output(cls, processed)
+        output: str = cls.format_output(processed)
         print(output)
 
 
 def main() -> None:
-    print_message([1, 2, 3, 4, 5], NumericProcessor)
-    print_message("Hello Nexus World", TextProcessor)
-    print_message(None, LogProcessor)
+    num_proc: DataProcessor = NumericProcessor()
+    text_proc: DataProcessor = TextProcessor()
+    log_proc: DataProcessor = LogProcessor()
+
+    print_message([1, 2, 3, 4, 5], num_proc)
+    print_message("Hello Nexus World", text_proc)
+    print_message(None, log_proc)
+
+    print("\n=== Polymorphic Processing Demo ===")
+    print("\nProcessing multiple data types through same interface...")
+    data_stream: list[tuple[DataProcessor | int | str]] =\
+        [(num_proc, [1, 2, 3]),
+         (text_proc, "Hello, World"),
+         (log_proc, "Test")]
+    i: int = 1
+    for proc, data in data_stream:
+        print(f"Result {i}: "
+              f"{proc.process(data)}")
+        i += 1
+    print("\nFoundation systems online. Nexus ready for advanced streams.")
 
 
 if (__name__ == "__main__"):
     print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===")
     main()
-    print("\n=== Polymorphic Processing Demo ===")
